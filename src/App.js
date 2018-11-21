2748d2e251a17b6033017f256e9c58fa1cebdbb1
hien pham <freedom9x@gmail.com>
2018-11-21 13:42:50 +0700

    define app layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div id="app" className="App">
+        <section id="app-header"> </section>
+        <section id="app-profile"> </section>
+        <section id="app-message-center"> </section>
+        <section id="app-news"> </section>
       </div>
     );
   }
